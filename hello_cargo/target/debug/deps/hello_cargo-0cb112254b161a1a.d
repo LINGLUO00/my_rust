eb4ceecdc9cfb5667e2d8b8a0586c81b3d92a246
LINGLUO00 <3425375449@qq.com>
2024-08-01 20:12:06 +0800

    24.8.1

---
+++ hello_cargo/target/debug/deps/hello_cargo-0cb112254b161a1a.d
@@ -0,0 +1,5 @@
+D:\my_rust\hello_cargo\target\debug\deps\libhello_cargo-0cb112254b161a1a.rmeta: src/main.rs
+
+D:\my_rust\hello_cargo\target\debug\deps\hello_cargo-0cb112254b161a1a.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+D:\my_rust\hello_cargo\target\debug\hello_cargo.exe: D:\my_rust\hello_cargo\src\main.rs
