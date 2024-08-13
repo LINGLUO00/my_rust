66da601c9dd0b046af45ecfff19d51039f407be3
LINGLUO00 <3425375449@qq.com>
2024-08-13 21:45:52 +0800

    24.8.13

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,14 +1,17 @@
-use std::cell::RefCell;
-use std::rc::Rc;
+#[derive(Debug)]
+struct WhatAboutThis<'a> {
+    name: String,
+    nickname: Option<&'a str>,
+}
+
 fn main() {
-    let s = Rc::new(RefCell::new("我很善变，还拥有多个主人".to_string()));
+    let mut tricky = WhatAboutThis {
+        name: "Annabelle".to_string(),
+        nickname: None,
+    };
+    tricky.nickname = Some(&tricky.name[..4]);
 
-    let s1 = s.clone();
-    let s2 = s.clone();
-    // let mut s2 = s.borrow_mut();
-    s2.borrow_mut().push_str(", oh yeah!");
-
-    println!("{:?}\n{:?}\n{:?}", s, s1, s2);
+    println!("{:?}", tricky);
 }
 
 
